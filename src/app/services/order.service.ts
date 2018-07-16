e62c4dc4c83ca1f0f356ff1150c6ee5a9f813b46
DESKTOP-96GBBGF\aruse <a.rusev@hotmail.com>
2018-07-16 12:19:33 +0300

    made Checkout form reactive

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,21 +1,24 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Router} from '@angular/router';
 import {CartService} from './cart.service';
 import {AuthService} from './auth.service';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {ToastrService} from 'ngx-toastr';
 import {environment} from '../../environments/environment';
-import {BehaviorSubject} from 'rxjs';
+import {BehaviorSubject, Observable} from 'rxjs';
 import {httpOptions} from '../shared/httpOptions';
+import {tap, catchError} from 'rxjs/operators';
 
 @Injectable()
 export class OrderService {
   orderCompleted: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+
   constructor(private router: Router,
               private cartService: CartService,
               private authService: AuthService,
               private toastrService: ToastrService,
-              private http: HttpClient) { }
+              private http: HttpClient) {
+  }
 
   submitOrder(address) {
     const products = this.cartService.getAllCartItemsRaw();
@@ -23,15 +26,12 @@ export class OrderService {
     const user = {
       id: userRaw._id,
       name: userRaw.name,
-    }
+    };
     const orderData = {user, products, address};
 
-    this.http.post(`${environment.api}/orders`, orderData, httpOptions).subscribe((result) => {
-      this.toastrService.success('Your order has been successfully submitted.');
-      this.orderCompleted.next(result);
-      this.router.navigate(['/order-completed']);
-    }, (error) => {
-      this.toastrService.error(error.error);
-    });
+    return this.http.post(`${environment.api}/orders`, orderData, httpOptions).pipe(
+      tap((result) => {
+        this.orderCompleted.next(result);
+      }));
   }
 }
--- src/app/shopping/shopping.module.ts
+++ src/app/shopping/shopping.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {CategoryComponent } from './category/category.component';
 import {CartComponent} from './cart/cart.component';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {ProductCardComponent} from './product/product-card/product-card.component';
 import {CreateProductComponent} from './product/create-product/create-product.component';
 import { SingleProductComponent } from './product/single-product/single-product.component';
@@ -17,6 +17,7 @@ import {SharedModule} from '../shared/shared.module';
     CommonModule,
     FormsModule,
     ShoppingRoutingModule,
+    ReactiveFormsModule,
     SharedModule,
   ],
   declarations: [
